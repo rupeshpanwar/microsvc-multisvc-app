3eab0ddcbba839ee67299345fc350d7e5b535f0e
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-19 19:18:32 +0530

    User Creation with Error handling

--- src/index.ts
+++ src/index.ts
@@ -26,19 +26,19 @@ app.use(errorHandler);
 
 const start = async () => {
   try {
-      await mongoose.connect('mongodb://auth-mongo-srv:27017/auth',{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      })
-      console.log('Connected to MongoDB');
-  } catch (error) {
-    console.log(error);
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
   }
-}
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
-});
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
 
-start()
+start();
