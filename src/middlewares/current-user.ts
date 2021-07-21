ebbebcc2ec3cc1058db48fa4f294df8caf600974
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-21 12:06:01 +0530

    Not Authorized Middleware

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,19 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-
 interface UserPayload {
   id: string;
   email: string;
 }
-
 declare global {
-  namespace Express {
+    namespace Express {
     interface Request {
       currentUser?: UserPayload;
     }
   }
 }
-
 export const currentUser = (
   req: Request,
   res: Response,
